f2f5a8fc522e59915fcbf539464a75d50f6d716d
David Juarez <DavidJuarez@Davids-MacBook-Pro.local>
2017-04-21 21:48:43 -0700

    added bootstrap and jquery

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Mechanic Site</title>
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-inverse" id="mainNav2">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+		      <span class="icon-bar"></span>
+		      <span class="icon-bar"></span>
+		      <span class="icon-bar"></span>
+		      </button>
+		      <a href="index.php">logo</a>
+		    </div>
+		    <div class="collapse navbar-collapse" id="myNavbar">
+		      <ul class="nav navbar-nav">
+		        <li><a id ="about" class ="design" href="graphicDesign.php">Design</a></li>
+		        <li><a id ="about" href="webDevelopment.php">Web Development</a></li>
+		          <li class="dropdown">
+		          <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user"></span> My Account<span class="caret"></span></a>
+		          <ul class="dropdown-menu">
+		            <li><a href="register.php">Sign Up</a></li>
+		            <li><a href="login.php">Login</a></li>
+		            <li><a href="delete.php">Delete Account</a></li>
+		            <li><a href="logout.php">Logout</a></li>
+		          </ul>
+		        </li>
+		      </ul>
+		    </div>
+		  </div>
+		</nav>
+		<div class="container">
+
+		</div>
+	</body>
+	<srcipt src="js/bootstrap.min.js"></srcipt>
+
+	<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+	<script src="js/script.js"></script>
+</html>
