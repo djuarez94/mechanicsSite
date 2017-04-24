58547441cfc79e2babe181c96c09938dccad1bb7
David Juarez <DavidJuarez@Davids-MacBook-Pro.local>
2017-04-24 13:40:09 -0700

    added parallax effect and boostrap nav to index.php

--- index.php
+++ index.php
@@ -8,39 +8,31 @@
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 	</head>
 	<body>
-		<nav class="navbar navbar-inverse" id="mainNav2">
-		  <div class="container-fluid">
-		    <div class="navbar-header">
-		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-		      <span class="icon-bar"></span>
-		      <span class="icon-bar"></span>
-		      <span class="icon-bar"></span>
-		      </button>
-		      <a href="index.php">logo</a>
-		    </div>
-		    <div class="collapse navbar-collapse" id="myNavbar">
-		      <ul class="nav navbar-nav">
-		        <li><a id ="about" class ="design" href="graphicDesign.php">Design</a></li>
-		        <li><a id ="about" href="webDevelopment.php">Web Development</a></li>
-		          <li class="dropdown">
-		          <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user"></span> My Account<span class="caret"></span></a>
-		          <ul class="dropdown-menu">
-		            <li><a href="register.php">Sign Up</a></li>
-		            <li><a href="login.php">Login</a></li>
-		            <li><a href="delete.php">Delete Account</a></li>
-		            <li><a href="logout.php">Logout</a></li>
-		          </ul>
-		        </li>
-		      </ul>
-		    </div>
-		  </div>
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+				 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				   <span class="icon-bar"></span>
+				   <span class="icon-bar"></span>
+				   <span class="icon-bar"></span>
+				 </button>
+				 <a class="navbar-brand" href="#">WebSiteName</a>
+				</div>
+				<div class="collapse navbar-collapse" id="myNavbar">
+					<ul class="nav navbar-nav">
+					   <li class="active"><a href="#">Home</a></li>
+					   <li><a href="#">About Us</a></li>
+					   <li><a href="#">Contact</a></li>
+					   <li><a href="#">Services</a></li>
+					</ul>
+				</div>
+			</div>
 		</nav>
-		<div class="container">
+		<div class="parallax"></div>
 
-		</div>
 	</body>
-	<srcipt src="js/bootstrap.min.js"></srcipt>
 
-	<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+	<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script src="js/script.js"></script>
 </html>
