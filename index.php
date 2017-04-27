422090a499e57b48b09e803ed8c7cfcdc1c82e31
David Juarez <DavidJuarez@Davids-MacBook-Pro.local>
2017-04-26 18:51:01 -0700

    added google fonts to text headers for all pages

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 		<title>Mechanic Site</title>
 		<link rel="stylesheet" href="css/style.css">
 		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Rajdhani" rel="stylesheet"> 
 	</head>
 	<body>
 		<nav class="navbar navbar-inverse">
