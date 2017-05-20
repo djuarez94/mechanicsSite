74daa1ae30a01332ab24d48bb0c0b4cd29406768
David Juarez <DavidJuarez@192.168.1.8>
2017-05-19 19:17:37 -0700

    created footer.php and removed footer code from every page and added to footer.php

--- about.php
+++ about.php
@@ -58,26 +58,4 @@
 				<img src="img/Toyota_Logo.png" width="100px" style="padding-right: 10px; padding-left: 10px;">
 			</div>
 		</div>
-		<footer>
-			<img class="footerLogo" src="img/mainLogo_Lrg.png">
-			<div class="email">
-				<i class="fa fa-envelope" aria-hidden="true" style="font-size: 2em;"></i>
-				<a href="mailto:randomEmail@email.com"><p>randomEmail@email.com</p></a>
-			</div>
-			<div class="number">
-				<i class="fa fa-mobile" aria-hidden="true" style="font-size: 2.5em;"></i>
-				<a href="tel:1-675-485-9384"><p>675-485-9384</p></a>
-			</div>
-			<div class="socialLinks">
-				<a href="https://facebook.com"><i id="socialLinks" class="fa fa-facebook-square" aria-hidden="true"></i></a>
-				<a href="https://instagram.com"><i id="socialLinks" class="fa fa-instagram" aria-hidden="true"></i></a>
-				<a href="https://twitter.com"><i id="socialLinks" class="fa fa-twitter-square" aria-hidden="true"></i></a>
-				<p class="copyright">&copy; <?php echo date('Y'); ?> Pepe's Auto Shop Solutions.<br>All rights reserved.</p>
-			</div>
-		</footer>
-	</body>
-
-	<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	<script src="js/script.js"></script>
-</html>
+		<?php include 'footer.php' ?>
--- services.php
+++ services.php
@@ -86,26 +86,4 @@
 			<h2>Specials!</h2>
 			<p>Don't have time to make it to our auto shops, or your automobile's problem prevents you from making it to our shops? No worries. We have a special going on this month where our mechanics enjoy the privelege of coming to you and finding a solution to your automobile's problems! Our prices for this special depends on the problem plus the area where the service will be done. So please give us a call to find out more!</p>
 		</div>
-		<footer>
-			<img class="footerLogo" src="img/mainLogo_Lrg.png">
-			<div class="email">
-				<i class="fa fa-envelope" aria-hidden="true" style="font-size: 2em;"></i>
-				<a href="mailto:randomEmail@email.com"><p>randomEmail@email.com</p></a>
-			</div>
-			<div class="number">
-				<i class="fa fa-mobile" aria-hidden="true" style="font-size: 2.5em;"></i>
-				<a href="tel:1-675-485-9384"><p>675-485-9384</p></a>
-			</div>
-			<div class="socialLinks">
-				<a href="https://facebook.com"><i id="socialLinks" class="fa fa-facebook-square" aria-hidden="true"></i></a>
-				<a href="https://instagram.com"><i id="socialLinks" class="fa fa-instagram" aria-hidden="true"></i></a>
-				<a href="https://twitter.com"><i id="socialLinks" class="fa fa-twitter-square" aria-hidden="true"></i></a>
-				<p class="copyright">&copy; <?php echo date('Y'); ?> Pepe's Auto Shop Solutions.<br>All rights reserved.</p>
-			</div>
-		</footer>
-	</body>
-
-	<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	<script src="js/script.js"></script>
-</html>
+		<?php include 'footer.php' ?>
